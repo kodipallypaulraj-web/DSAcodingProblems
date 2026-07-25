2a01596245ee5930b082bd8114484a4719942c53
Paulraj Kodipally <kodipallypaulraj@gmail.com>
2026-07-25 11:33:17 +0530

    Time: 0 ms (100%), Space: 10.1 MB (31.4%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+
+        for (char c : s) {
+            if (isalnum(c))
+                str += tolower(c);
+        }
+
+        int left = 0, right = str.size() - 1;
+
+        while (left < right) {
+            if (str[left] != str[right])
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
+        
+    }
+};
