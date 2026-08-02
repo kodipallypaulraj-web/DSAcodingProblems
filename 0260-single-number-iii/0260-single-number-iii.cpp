99dedeefc55e558285a474feae14b42308965f34
Paulraj Kodipally <kodipallypaulraj@gmail.com>
2026-08-02 23:49:37 +0530

    Time: 0 ms (100%), Space: 14.9 MB (9.57%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+         unordered_map<int,int> freq;
+        vector<int> ans;
+
+        for(int num : nums)
+            freq[num]++;
+
+        for(auto it : freq){
+            if(it.second == 1)
+                ans.push_back(it.first);
+        }
+
+        return ans;
+    }
+};
